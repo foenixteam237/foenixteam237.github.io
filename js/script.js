696d171d35ec6efe40359a5e2d3b356706c5d2f6
foenixteam237 <91803294+foenixteam237@users.noreply.github.com>
2022-08-31 00:25:39 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Graphic Designer","Android developer","Java developer"],
+    typedSpeed:400,
+    BackSpeed:920,
+    loop:true
+})
